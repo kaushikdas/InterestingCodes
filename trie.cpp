930d8d72564102ed176dbdbc2cbe0aef17c57d61
Kaushik Das <mr.kaushik.das@gmail.com>
2018-07-29 14:52:14 +0530

    Added logestPrefix API

--- trie.cpp
+++ trie.cpp
@@ -1,5 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -21,6 +19,17 @@ static char* ztrdup(char *t)
   int l = ztrlen(t) + 1;
   char *s = (char*) malloc(l * sizeof(char));
   ztrcpy(s, t);
+  return s;
+}
+
+// returns a new char string that is a substring of t
+// from t[l] to t[r - 1]
+static char* ztrsub(char *t, int l, int r)
+{
+  char *s = (char*)malloc((r - l + 1) * sizeof(char));
+  for (register int i = l; i < r; ++i)
+    s[i] = t[i];
+  s[r] = '\0';
   return s;
 }
 
@@ -63,7 +72,7 @@ public:
   void iterateQ()
   {
     for (register int t = f; t < r; ++t) 
-      printf("(%s, %d)\n", elm[t].key, elm[t].val);
+      printf("  (%s, %d)\n", elm[t].key, elm[t].val);
   };
 
   void flushQ()
@@ -73,6 +82,7 @@ public:
         free(elm[i].key); elm[i].key = nullptr;
       }
     }
+    f = r = 0;
   };
 };
 
@@ -147,18 +157,27 @@ class Trie
         x->isKey = false; sz--;
       }
       else {
-        char c = key[d] - 'a';
-        x->next[c] = remove(x->next[c], d + 1);
+        printf("  <TRIE_INFO> "
+               "remove - key \"%s\" is not found!\n", key);
       }
-
-      // remove subtrie rooted at x if it is completely empty
-      if (x->isKey) return x;
-      for (char c = 0; c < R; ++c)
-        if (x->next[c] != nullptr)
-          return x;
-
-      delete x; return nullptr;
     }
+    else {
+      char c = key[d] - 'a';
+      if (x->next[c])
+        x->next[c] = remove(x->next[c], d + 1);
+      else
+        printf("  <TRIE_INFO> "
+               "remove - key \"%s\" is not found!\n", key);
+    }
+
+    // remove subtrie rooted at x if it is completely empty
+    if (x->isKey) return x;
+    for (char c = 0; c < R; ++c)
+      if (x->next[c] != nullptr)
+        return x;
+
+    delete x;
+    return nullptr;
   };
 
   // IMPORTANT: prefix MUST be large enough be to hold the
@@ -166,6 +185,7 @@ class Trie
   // results: out param
   void collect(Nd *x, char *prefix, IterableQ& results)
   {
+    if (x == nullptr) return;
     if (x->isKey) results.enQ(prefix, x->val);
     int l = ztrlen(prefix);
     for (char c = 0; c < R; ++c) {
@@ -180,6 +200,15 @@ class Trie
     }
   };
 
+  int longestPrefix(Nd *x, int d, int prefixLen)
+  {
+    if (x == nullptr) return prefixLen;
+    if (x->isKey) prefixLen = d;
+    if (d == currentKeyLen) return prefixLen;
+    char c = key[d] - 'a';
+    return longestPrefix(x->next[c], d + 1, prefixLen);
+  };
+
 public:
   Trie() { root = nullptr; sz = 0; };
 
@@ -190,7 +219,7 @@ public:
   void add(char *key, int val)
   {
     if (key == nullptr) {
-      printf("[EXCEPTION] add - key is null!\n");
+      printf("  <TRIE_EXCEPTION> add - key is null!\n");
       return;
     }
 
@@ -202,14 +231,14 @@ public:
   int search(char *key)
   {
     if (key == nullptr) {
-      printf("[EXCEPTION] search - key is null!\n");
+      printf("  <TRIE_EXCEPTION> search - key is null!\n");
       return -1;
     }
 
     setSearchKey(key);
     Nd *x = search(root, 0);
     if (x == nullptr) {
-      printf("[RESULT] key %s not found.\n", key);
+      printf("  <TRIE_INFO> key %s not found.\n", key);
       return -1;
     }
     return x->val;
@@ -221,7 +250,7 @@ public:
   void remove(char *key)
   {
     if (key == nullptr)
-      printf("[EXCEPTION] remove - key is null!\n");
+      printf("  <TRIE_EXCEPTION> remove - key is null!\n");
     setSearchKey(key);
     root = remove(root, 0);
   };
@@ -237,16 +266,33 @@ public:
   // results: out param
   void keysWithPrefix(char *prefix, IterableQ& results)
   {
+    if (prefix == nullptr)
+      printf("  <TRIE_EXCEPTION> keyWithPrefix - prefix is null!\n");
     setSearchKey(prefix);
     Nd *x = search(root, 0);
     // x is the nore till which the prefix exactly matches
     collect(x, prefix, results);
   };
+
+  // Finds the key in the trie that longest prefix of the
+  // input string s
+  char* longestPrefix(char *s)
+  {
+    if (s == nullptr)
+      printf("  <TRIE_EXCEPTION> longestPrefix - s is null!\n");
+    setSearchKey(s);
+    int l = longestPrefix(root, 0, -1);
+    if (l == -1) return nullptr;
+    else {
+      char *t = ztrsub(s, 0, l);
+      return t;
+    }
+  };
 };
 
 int main()
 {
-  printf("TRIE\n");
+  printf("TRIE_MAIN\n");
 
   freopen("TrieInput.txt", "r", stdin);
 
@@ -258,23 +304,38 @@ int main()
   char key[10][20] = { 0 };
   for (int i = 0; i < n; ++i) {
     scanf(" %[^\n]", key[i]);
-    printf("[TRIE] adding (%s, %d)...\n", key[i], i);
+    printf("[TRIE_MAIN] adding (%s, %d)...\n", key[i], i);
     trie.add(key[i], i);
   }
-  printf("[TRIE] added %d keys.\n", trie.size());
+  printf("[TRIE_MAIN] added %d keys.\n", trie.size());
   // print the (key, val) pairs in the trie
   for (int i = 0; i < n; ++i) {
     int val = trie.search(key[i]);
-    printf("[TRIE] Found (%s, %d).\n", key[i], val);
+    printf("[TRIE_MAIN] Found (%s, %d).\n", key[i], val);
   }
 
+  printf("[TRIE_MAIN] Search a non-existing key \"false\"...\n");
   int val = trie.search("false");
 
+  printf("[TRIE_MAIN] Key-Value pairs in the trie.\n");
   IterableQ results;
   trie.keys(results);
   results.iterateQ();
   results.flushQ();
 
+  printf("[TRIE_MAIN] Removing keys \"sea\" and \"sha\" from the trie.\n");
+  trie.remove("sea");
+  trie.remove("sha");
+
+  printf("[TRIE_MAIN] After removal trie has %d keys.\n", trie.size());
+  printf("[TRIE_MAIN] Key-Value pairs in the trie after removal.\n");
+  trie.keys(results);
+  results.iterateQ();
+  results.flushQ();
+
+  char *t = trie.longestPrefix("shellsort");
+  printf("[TRIE] Longest prefix of \"shellsort\" is \"%s\".\n", t);
+  free(t);
   return 0;
 }
 
@@ -292,29 +353,41 @@ seashore
 
 OUTPUT
 ------
-TRIE
-[TRIE] adding (she, 0)...
-[TRIE] adding (sells, 1)...
-[TRIE] adding (sea, 2)...
-[TRIE] adding (shells, 3)...
-[TRIE] adding (by, 4)...
-[TRIE] adding (the, 5)...
-[TRIE] adding (seashore, 6)...
-[TRIE] added 7 keys.
-[TRIE] Found (she, 0).
-[TRIE] Found (sells, 1).
-[TRIE] Found (sea, 2).
-[TRIE] Found (shells, 3).
-[TRIE] Found (by, 4).
-[TRIE] Found (the, 5).
-[TRIE] Found (seashore, 6).
-[RESULT] key false not found.
-(by, 4)
-(sea, 2)
-(seashore, 6)
-(sells, 1)
-(she, 0)
-(shells, 3)
-(the, 5)
-
+TRIE_MAIN
+[TRIE_MAIN] adding (she, 0)...
+[TRIE_MAIN] adding (sells, 1)...
+[TRIE_MAIN] adding (sea, 2)...
+[TRIE_MAIN] adding (shells, 3)...
+[TRIE_MAIN] adding (by, 4)...
+[TRIE_MAIN] adding (the, 5)...
+[TRIE_MAIN] adding (seashore, 6)...
+[TRIE_MAIN] added 7 keys.
+[TRIE_MAIN] Found (she, 0).
+[TRIE_MAIN] Found (sells, 1).
+[TRIE_MAIN] Found (sea, 2).
+[TRIE_MAIN] Found (shells, 3).
+[TRIE_MAIN] Found (by, 4).
+[TRIE_MAIN] Found (the, 5).
+[TRIE_MAIN] Found (seashore, 6).
+[TRIE_MAIN] Search a non-existing key "false"...
+  <TRIE_INFO> key false not found.
+[TRIE_MAIN] Key-Value pairs in the trie.
+  (by, 4)
+  (sea, 2)
+  (seashore, 6)
+  (sells, 1)
+  (she, 0)
+  (shells, 3)
+  (the, 5)
+[TRIE_MAIN] Removing keys "sea" and "sha" from the trie.
+<TRIE_INFO> remove - key "sha" is not found!
+[TRIE_MAIN] After removal trie has 6 keys.
+[TRIE_MAIN] Key-Value pairs in the trie after removal.
+  (by, 4)
+  (seashore, 6)
+  (sells, 1)
+  (she, 0)
+  (shells, 3)
+  (the, 5)
+[TRIE] Longest prefix of "shellsort" is "shells".
 */
