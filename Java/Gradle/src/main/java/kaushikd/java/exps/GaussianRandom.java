3ee9043a9f956f3564efb34ddf0ccd89a6e80c3f
Kaushik Das <mr.kaushik.das@gmail.com>
2020-05-30 19:43:31 +0530

    - Added lombok support (used @NoArgsConstructor, @ToString and @Slf4j)   - Included lombok plugin to build.gradle (To use lombok properly in IntelliJ we need to install lombok plugin to IntelliJ (File -> Settings -> Plugin) and enable Java Annotation processor

--- Java/Gradle/src/main/java/kaushikd/java/exps/GaussianRandom.java
+++ Java/Gradle/src/main/java/kaushikd/java/exps/GaussianRandom.java
@@ -1,17 +1,23 @@
 package kaushikd.java.exps;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 
+@NoArgsConstructor
+@ToString
+@Slf4j
 public class GaussianRandom {
     public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger(GaussianRandom.class);
+        //Logger logger = LoggerFactory.getLogger(GaussianRandom.class);
 
-        logger.info("GaussianRandom");
+        log.info("GaussianRandom Class");
         double u = Math.random();
         double v = Math.random();
 
         double r = Math.sin(2*Math.PI*v)*Math.sqrt(-2*Math.log(u));
-        logger.info("Random number: " + r);
+        log.info("Random number: " + r);
     }
 }
