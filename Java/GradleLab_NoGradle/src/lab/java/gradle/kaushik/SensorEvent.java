afd7d25ae5b42e7366aa8805ecdb89bf4cfa3aec
Kaushik Das <mr.kaushik.das@gmail.com>
2020-07-18 21:12:51 +0530

    Converting SensorEvent to JSON string using Google Gson library

--- Java/GradleLab_NoGradle/src/lab/java/gradle/kaushik/SensorEvent.java
+++ Java/GradleLab_NoGradle/src/lab/java/gradle/kaushik/SensorEvent.java
@@ -1,5 +1,8 @@
 package lab.java.gradle.kaushik;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class SensorEvent {
     String id;
     String capability;
@@ -45,10 +48,18 @@ public class SensorEvent {
         sensorEvent.setValue("active");
 
         System.out.println(sensorEvent.toString());
+
+        Gson json = new GsonBuilder().create();
+        String jsonEventString = json.toJson(sensorEvent);
+        System.out.println(jsonEventString);
     }
 }
 
 /*
  * Execution result
  * SensorEvent{id='100-101-102-103', capability='motionSensor', value='active'}
+ * {"id":"100-101-102-103","capability":"motionSensor","value":"active"}
+ *
+ * 1st one is normal string returned from our toString()
+ * 2nd one is the converted JSON string
  */
