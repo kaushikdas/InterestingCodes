85fcf89cebe244451465933fad4a2345eb5f6906
Kaushik Das <mr.kaushik.das@gmail.com>
2021-08-17 21:26:55 +0530

    vailla spring project

---
+++ Spring/sprint-in-simple-steps/src/main/java/com/kaushik/spring/basics/sprintinsimplesteps/SprintInSimpleStepsApplication.java
@@ -0,0 +1,15 @@
+package com.kaushik.spring.basics.sprintinsimplesteps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintInSimpleStepsApplication {
+
+  public static void main(String[] args) {
+
+  	SpringApplication.run(SprintInSimpleStepsApplication.class, args);
+
+  }
+
+}
---
+++ Spring/sprint-in-simple-steps/src/test/java/com/kaushik/spring/basics/sprintinsimplesteps/SprintInSimpleStepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaushik.spring.basics.sprintinsimplesteps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintInSimpleStepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
